9eb01ccaf8ae31f301d908a086f85c33ded8bb03
lailtlqdr1 <lailtlqodryah01@gmail.com>
2024-04-25 08:46:18 +0700

    Setting url, buat database, connect database

---
+++ application/views/V_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Lailatul</li>
+        <li>Nama Belakang : Qodariyah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> perum cileungsi hijau blok L2/22</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Dukun</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Tidur</li>
+        <li>Nonton</li>
+    </ul>
+</section
