b1014f45e5e5a73a20e0d940f7aab51726ea2c39
lailtlqdr1 <lailtlqodryah01@gmail.com>
2024-05-30 09:20:38 +0700

    membuat halaman login dan halaman admin

---
+++ application/views/template/aute_footer.php
@@ -0,0 +1,12 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('asset_login/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('asset_login/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('asset_login/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('asset_login/'); ?>js/sb-admin-2.min.js"></script>
+<script>
+    $('.alert-message').alert().delay(3000).slideUp('slow');
+</script>
+</body>
+</html>
